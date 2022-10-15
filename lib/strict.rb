729fbfbb42e904aef4e5fe2614d6d423414ad6bc
Kyle Thompson <me@kkt.dev>
2022-10-15 19:31:49 -0400

    Add configuration to sample validation (for prod environments)

--- lib/strict.rb
+++ lib/strict.rb
@@ -6,4 +6,14 @@ loader.setup
 
 module Strict
   ISSUE_TRACKER = "https://github.com/kylekthompson/strict/issues"
+
+  class << self
+    def configuration
+      @configuration ||= Strict::Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+  end
 end
