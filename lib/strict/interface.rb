c105c57c39cb7246785dbfc9c4cb94677272cb3d
Kyle Thompson <me@kkt.dev>
2022-10-20 13:35:04 -0400

    Interfaces should have supported parameterless methods

--- lib/strict/interface.rb
+++ lib/strict/interface.rb
@@ -13,13 +13,19 @@ module Strict
 
     def expose(method_name, &block)
       sig = sig(&block)
-      parameter_list = sig.parameters.map { |parameter| "#{parameter.name}:" }.join(", ")
-      argument_list = sig.parameters.map { |parameter| "#{parameter.name}: #{parameter.name}" }.join(", ")
+      parameter_list = [
+        *sig.parameters.map { |parameter| "#{parameter.name}:" },
+        "&block"
+      ].join(", ")
+      argument_list = [
+        *sig.parameters.map { |parameter| "#{parameter.name}: #{parameter.name}" },
+        "&block"
+      ].join(", ")
 
       module_eval(<<~RUBY, __FILE__, __LINE__ + 1)
-        def #{method_name}(#{parameter_list}, &block)              # def method_name(one:, two:, three:, &block)
-          implementation.#{method_name}(#{argument_list}, &block)  #   implementation.method_name(one: one, two: two, three: three, &block)
-        end                                                        # end
+        def #{method_name}(#{parameter_list})              # def method_name(one:, two:, three:, &block)
+          implementation.#{method_name}(#{argument_list})  #   implementation.method_name(one: one, two: two, three: three, &block)
+        end                                                # end
       RUBY
     end
   end
